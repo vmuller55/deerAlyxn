fab6072156c4fddedb06bbf7c1e86d3974080cee
Vincent Muller <v.muller55000@gmail.com>
2023-09-21 23:47:02 +0200

    gh pages

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,27 +1,30 @@
 import './work.css'
 import StainWork from '../../assets/stainFive.png'
+import ongle1 from '../../assets/ongle1.jpg'
+import ongle2 from '../../assets/ongle2.jpg'
+import ongle3 from '../../assets/ongle3.jpg'
+import ongle4 from '../../assets/ongle4.jpg'
+import ongle5 from '../../assets/ongle5.jpg'
+import ongle6 from '../../assets/ongle6.jpg'
+import ongle7 from '../../assets/ongle7.jpg'
+import ongle8 from '../../assets/ongle8.jpg'
+
 
 const Work = () => {
-
-    const data = [
-        '/deerAlyxn/assets/ongle1.jpg',
-        '../../assets/ongle2.jpg',
-        '../../assets/ongle3.jpg',
-        '../../assets/ongle4.jpg',
-        '../../assets/ongle5.jpg',
-        '../../assets/ongle6.jpg',
-        '../../assets/ongle7.jpg',
-        '../../assets/ongle8.jpg',
-    ]
 
     return(
         <div className="workContainer">
             <div className="workColorBloc">
                 <h2>Mon travail</h2>
                 <div className="photoTab">
-                    {data.map((image, index) => {
-                        return <img src={image} key={index} className='workImg'></img>
-                    })}
+                    <img src={ongle1} className='workImg'></img>
+                    <img src={ongle2} className='workImg'></img>
+                    <img src={ongle3} className='workImg'></img>
+                    <img src={ongle4} className='workImg'></img>
+                    <img src={ongle5} className='workImg'></img>
+                    <img src={ongle6} className='workImg'></img>
+                    <img src={ongle7} className='workImg'></img>
+                    <img src={ongle8} className='workImg'></img>
                 </div>
             </div>
             <div className="workStain">
