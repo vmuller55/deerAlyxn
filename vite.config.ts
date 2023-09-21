8ab8e0df3392be154b9d3ed813e51b547fa1e10b
Vincent Muller <v.muller55000@gmail.com>
2023-09-21 23:34:12 +0200

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : '/deerAlyxn/',
   plugins: [react()],
-  base : 'http://vmuller55.github.io/deerAlyxn/'
+  
 })
